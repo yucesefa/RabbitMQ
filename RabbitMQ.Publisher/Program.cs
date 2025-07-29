3818f7a6a4deada2e80e1119b931ae85a63326a3
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-07-29 14:52:37 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+namespace RabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory();
+            factory.Uri =  new Uri("amqps://imoearbm:SKLx7G1hs1_fNyGVw3MMDlc6bNLihq8x@woodpecker.rmq.cloudamqp.com/imoearbm");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            string message = "Things Of Quality Have No Fear Of Time";
+
+            var messageBody = System.Text.Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty,"hello-queue",null,messageBody);
+            Console.WriteLine("Message sent");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+namespace RabbitMQ.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://imoearbm:SKLx7G1hs1_fNyGVw3MMDlc6bNLihq8x@woodpecker.rmq.cloudamqp.com/imoearbm");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //channel.QueueDeclare("hello-queue", true, false, false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue", true, consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = System.Text.Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Received Message = " + message);
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
